20e946a3e61b526fbfb346d4f0012cc60b4047ea
tomunizua <t.omunizua@alustudent.com>
2023-09-08 09:24:09 -0700

    8

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([A-Za-z0-9+]+)\]|\[to:([A-Za-z0-9+]+)\]|\[flags:([0-9:-]+)\]/).flatten.compact.join(',')
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(",")
